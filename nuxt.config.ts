1403de98eb054fdbea1941486be276da6e1e9a34
Александр <www.ovdey@yandex.ru>
2025-03-25 10:43:41 +0300

    add deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,11 +20,6 @@ export default defineNuxtConfig({
       ]
     }
   },
-  nitro: {
-    output: {
-      publicDir: 'docs'
-    }
-  },
   ssr: false,
   compatibilityDate: '2024-11-01',
   devtools: { enabled: false },
